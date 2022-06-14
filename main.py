149dd455f157826ec639add95236e55669284db8
Povladislav <vladpomalejko@gmail.com>
2022-06-14 22:07:51 +0300

    third commit

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def decorator_function(funct_to_wrap):
+    def wrapper():
+        print("Yo!")
+        funct_to_wrap()
+    return wrapper()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-    print('Changed!')
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@decorator_function
+def my_func():
+    print('Hello world!')
+my_func()
