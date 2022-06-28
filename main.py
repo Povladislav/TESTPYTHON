21836acd61a69f0da50f56b8c582c5ccbf62f69a
Povladislav <vladpomalejko@gmail.com>
2022-06-28 21:28:06 +0300

    commit

--- main.py
+++ main.py
@@ -1,11 +1,3 @@
-
-def decorator_function(funct_to_wrap):
-    def wrapper():
-        print("Yo!")
-        funct_to_wrap()
-    return wrapper()
-
-@decorator_function
-def my_func():
-    print('Hello world!')
-my_func()
+class Test:
+    __test = 0
+print(Test.__test)
